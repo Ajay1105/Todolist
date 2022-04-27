65f08e4df6ae09022090a3447979e28701e2aad5
Ajay <aky11052003@gmail.com>
2022-04-27 13:15:36 +0530

    to do list

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-require('dotenv').config()
+require('dotenv').config();
 
 
 //mongo and mongoose
